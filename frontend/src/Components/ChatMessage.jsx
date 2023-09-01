9c1be1e7be627395a0b88f03ca6a7b462e450ad7
Mohammad Momin <mominmohammad111@gmail.com>
2023-09-02 00:18:03 +0530

    Added some css

--- frontend/src/Components/ChatMessage.jsx
+++ frontend/src/Components/ChatMessage.jsx
@@ -1,14 +1,22 @@
+import { useState } from 'react';
 import './ChatMessage.css'
-import {BiSolidUserCircle} from "react-icons/bi";
+import {AiFillLike} from "react-icons/ai";
 
 export default function ChatMessage({user,msg}){
+    const [count,setCount] = useState(0);
     return(
         <div className="ChatMessage-div">
             <div className="ChatMessage-UserInfo-div">
             <div className="ChatMessage-UserIcon">{user[0]}</div>
             <h3>{user}</h3>
             </div>
+            <div className="ChatMessage-Message-maindiv">
             <div className="ChatMessage-Message-div">{msg}</div>
+            <div 
+            style={{color:"#1483fa",cursor:"pointer"}} 
+            onClick={()=>setCount(prev=>prev+1)}><AiFillLike /></div>
+            <h5>{count}</h5>
+            </div>
           </div>
     )
 }
